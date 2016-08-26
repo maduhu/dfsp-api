e2d5a88ba53163c12b76b44e11cf15c7a2c1ed0a
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-08-26 14:27:28 +0300

    feat: mock push transfer

--- httpclient/transfer.js
+++ httpclient/transfer.js
@@ -3,5 +3,14 @@ module.exports = {
   createPort: require('ut-port-jsonrpc'),
   url: 'http://localhost:8018',
   namespace: ['transfer'],
-  method: 'post'
+  method: 'post',
+  'transfer.push.execute': function (msg) {
+    return {
+      payload: {
+        result: {
+          fulfillment: 'tx-mock'
+        }
+      }
+    }
+  }
 }
