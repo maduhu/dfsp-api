e1de703ca4d7386f643a3122ba97d5e8f5bdce99
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-09-12 16:27:43 +0300

    fix: call transfer.push.execute remotely

--- httpclient/transfer.js
+++ httpclient/transfer.js
@@ -3,14 +3,5 @@ module.exports = {
   createPort: require('ut-port-jsonrpc'),
   url: 'http://localhost:8018',
   namespace: ['transfer'],
-  method: 'post',
-  'transfer.push.execute': function (msg) {
-    return {
-      payload: {
-        result: {
-          fulfillment: 'tx-mock'
-        }
-      }
-    }
-  }
+  method: 'post'
 }
