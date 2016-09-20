0d5dd782cba737f4c9f490d4b08028acf48f636b
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-09-20 19:04:45 +0300

    feat: add identity.add method encapsulating bussiness logic for adding user, account and subscription

--- service/identity.js
+++ service/identity.js
@@ -1,7 +1,64 @@
+var importMethod
 module.exports = {
+  init: function (b) {
+    importMethod = b.importMethod.bind(b)
+  },
   check: function () {
     return {
       'permission.get': ['*']
     }
+  },
+  add: function (msg, $meta) {
+    var reversals = []
+    $meta.method = 'directory.user.add'
+    return importMethod($meta.method)(msg, $meta)
+      .then((res) => {
+        msg.actorId = '' + res.actorId
+        reversals.push({
+          method: 'directory.user.remove',
+          msg: {
+            actorId: res.actorId
+          }
+        })
+        if (msg.phoneNumber) {
+          $meta.method = 'subscription.subscription.add'
+          return importMethod($meta.method)(msg, $meta)
+            .then((res) => {
+              reversals.push({
+                method: 'subscription.subscription.remove',
+                msg: {
+                  subscriptionId: res.subscriptionId
+                }
+              })
+            })
+        }
+      })
+      .then(() => {
+        if (msg.accountId) {
+          $meta.method = 'account.account.add'
+          return importMethod($meta.method)(msg, $meta)
+          .then((res) => {
+            reversals.push({
+              method: 'account.account.remove',
+              msg: {
+                accountId: res.accountId
+              }
+            })
+          })
+        }
+      })
+      .then(() => (msg))
+      .catch((err) => {
+        if (reversals.length) {
+          return Promise.all(reversals.map((reversal) => {
+            $meta.method = reversal.method
+            return importMethod($meta.method)(reversal.msg, $meta)
+          }))
+          .then(() => {
+            throw err
+          })
+        }
+        throw err
+      })
   }
 }
