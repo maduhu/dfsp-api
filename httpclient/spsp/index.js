3f9cce68bcebdf24a26f5f94421e830bb4ff6836
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-25 14:55:07 +0200

    fix: add spsp.invoice.get method

--- httpclient/spsp/index.js
+++ httpclient/spsp/index.js
@@ -10,11 +10,24 @@ module.exports = {
   parseResponse: false,
   requestTimeout: 300000,
   method: 'get',
+  uri: '',
+  'spsp.invoice.get.request.send': function(msg) {
+    return {
+      url: msg.url
+    }
+  },
+  'spsp.invoice.get.error.receive': function(msg) {
+    return msg
+  },
+  'spsp.invoice.get.response.receive': function(msg) {
+    return msg.payload
+  },
   'spsp.payee.get.request.send': function (msg, $meta) {
     return this.bus.importMethod('ist/directory.user.get')({
       identifier: msg.identifier
     })
     .then((res) => {
+      $meta.submissionUrl = res.spspReceiver
       return {
         url: res.spspReceiver,
         uri: '/receivers/' + msg.identifier
@@ -22,6 +35,8 @@ module.exports = {
     })
   },
   'spsp.payee.get.response.receive': function (msg, $meta) {
+    msg.payload.submissionUrl = $meta.submissionUrl
+    delete $meta.submissionUrl
     return msg.payload
   }
 }
