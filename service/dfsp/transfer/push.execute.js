e6af48dfe49c3eb5524a9e55f5c5cd3a5f38e650
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-04-11 13:31:39 +0300

    fix: unify transfer.push.execute approach for both environments

--- service/dfsp/transfer/push.execute.js
+++ service/dfsp/transfer/push.execute.js
@@ -1,24 +1,35 @@
 module.exports = {
   'push.execute': function (msg, $meta) {
-    var promise
-    var params = {
+    return this.bus.importMethod('spsp.transfer.transfer.execute')({
       receiver: msg.receiver,
       sourceAccount: msg.sourceAccount,
       destinationAmount: Number(msg.destinationAmount).toFixed(2),
       memo: msg.memo || '',
-      sourceIdentifier: msg.sourceIdentifier
-    }
-    if ((this.bus.config.cluster || '').endsWith('-test')) {
-      params.sourceAmount = (Number(msg.destinationAmount) + Number(msg.fee || 0)).toFixed(2)
-      promise = this.bus.importMethod('spsp.transfer.transfer.execute')(params)
-    } else {
-      promise = this.bus.importMethod('spsp.transfer.transfer.setup')(params)
-      .then((result) => {
-        result.destinationAmount = Number(result.destinationAmount).toFixed(2)
-        result.sourceAmount = Number(result.sourceAmount).toFixed(2)
-        return this.bus.importMethod('spsp.transfer.transfer.execute')(result)
-      })
-    }
-    return promise.then((result) => this.config.exec(result, $meta))
+      sourceIdentifier: msg.sourceIdentifier,
+      sourceAmount: (Number(msg.destinationAmount) + Number(msg.fee || 0)).toFixed(2)
+    })
+    .then((result) => {
+      return this.config.exec(result, $meta)
+    })
+    // var promise
+    // var params = {
+    //   receiver: msg.receiver,
+    //   sourceAccount: msg.sourceAccount,
+    //   destinationAmount: Number(msg.destinationAmount).toFixed(2),
+    //   memo: msg.memo || '',
+    //   sourceIdentifier: msg.sourceIdentifier
+    // }
+    // if ((this.bus.config.cluster || '').endsWith('-test')) {
+    //   params.sourceAmount = (Number(msg.destinationAmount) + Number(msg.fee || 0)).toFixed(2)
+    //   promise = this.bus.importMethod('spsp.transfer.transfer.execute')(params)
+    // } else {
+    //   promise = this.bus.importMethod('spsp.transfer.transfer.setup')(params)
+    //   .then((result) => {
+    //     result.destinationAmount = Number(result.destinationAmount).toFixed(2)
+    //     result.sourceAmount = Number(result.sourceAmount).toFixed(2)
+    //     return this.bus.importMethod('spsp.transfer.transfer.execute')(result)
+    //   })
+    // }
+    // return promise.then((result) => this.config.exec(result, $meta))
   }
 }
