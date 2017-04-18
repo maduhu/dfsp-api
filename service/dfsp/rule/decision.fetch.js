04d2865946156113fa4342b9ba586caa27dcd1ff
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-04-18 16:49:39 +0300

    fix: rules conditions - account current transaction

--- service/dfsp/rule/decision.fetch.js
+++ service/dfsp/rule/decision.fetch.js
@@ -29,22 +29,22 @@ module.exports = {
                 if (rule.limit.maxAmount && msg.amount > rule.limit.maxAmount) {
                   throw error.maxAmount({limit: rule.limit.maxAmount})
                 }
-                if (rule.limit.maxAmountDaily && transfers.amountDaily > rule.limit.maxAmountDaily) {
+                if (rule.limit.maxAmountDaily && +transfers.amountDaily + +msg.amount > rule.limit.maxAmountDaily) {
                   throw error.maxAmountDaily({ limit: rule.limit.maxAmountDaily })
                 }
-                if (rule.limit.maxCountDaily && transfers.countDaily > rule.limit.maxCountDaily) {
+                if (rule.limit.maxCountDaily && transfers.countDaily >= rule.limit.maxCountDaily) {
                   throw error.maxCountDaily({ limit: rule.limit.maxCountDaily })
                 }
-                if (rule.limit.maxAmountWeekly && transfers.amountWeekly > rule.limit.maxAmountWeekly) {
+                if (rule.limit.maxAmountWeekly && +transfers.amountWeekly + +msg.amount > rule.limit.maxAmountWeekly) {
                   throw error.maxAmountWeekly({ limit: rule.limit.maxAmountWeekly })
                 }
-                if (rule.limit.maxCountWeekly && transfers.countWeekly > rule.limit.maxCountWeekly) {
+                if (rule.limit.maxCountWeekly && transfers.countWeekly >= rule.limit.maxCountWeekly) {
                   throw error.maxCountWeekly({ limit: rule.limit.maxCountWeekly })
                 }
-                if (rule.limit.maxAmountMonthly && transfers.amountMonthly > rule.limit.maxAmountMonthly) {
+                if (rule.limit.maxAmountMonthly && +transfers.amountMonthly + +msg.amount > rule.limit.maxAmountMonthly) {
                   throw error.maxAmountMonthly({ limit: rule.limit.maxAmountMonthly })
                 }
-                if (rule.limit.maxCountMonthly && transfers.countMonthly > rule.limit.maxCountMonthly) {
+                if (rule.limit.maxCountMonthly && transfers.countMonthly >= rule.limit.maxCountMonthly) {
                   throw error.maxCountMonthly({ limit: rule.limit.maxCountMonthly })
                 }
                 return this.bus.importMethod('spsp.rule.decision.fetch')({
