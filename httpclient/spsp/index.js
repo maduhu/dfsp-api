3e5bc6f433f5deee1b51f840e11608df89e39bce
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-04-27 17:52:28 +0300

    fix: revert spsp.transfer.payee.get.request.send changes

--- httpclient/spsp/index.js
+++ httpclient/spsp/index.js
@@ -120,16 +120,22 @@ module.exports = {
     throw err
   },
   'spsp.transfer.payee.get.request.send': function (msg, $meta) {
-    return {
-      uri: '/query',
-      httpMethod: 'get',
-      headers: {
-        TraceID: uuid()
-      },
-      qs: {
-        identifier: msg.identifier
-      }
-    }
+    return this.bus.importMethod('ist.directory.user.get')({
+      identifier: msg.identifier
+    })
+      .then((res) => {
+        $meta.spspServer = res.spspReceiver
+        return {
+          uri: '/query',
+          httpMethod: 'get',
+          headers: {
+            TraceID: uuid()
+          },
+          qs: {
+            receiver: res.spspReceiver + '/receivers/' + msg.identifier
+          }
+        }
+      })
   },
   'spsp.transfer.payee.get.response.receive': function (msg, $meta) {
     msg.payload.spspServer = $meta.spspServer
