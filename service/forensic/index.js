a92155f42931c401fac8a522dd50a5167e50ba02
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-05-03 18:00:00 +0300

    fix: forensic logging websocket reconnect

--- service/forensic/index.js
+++ service/forensic/index.js
@@ -1,12 +1,13 @@
 var Ws = require('ws')
 var logMessage
+
 module.exports = {
   init: function (bus) {
-    if (bus.config.forensic && bus.config.forensic.client) {
+    if (bus.config.forensic) {
       var config = Object.assign({
         host: '127.0.0.1',
         protocol: 'ws'
-      }, bus.config.forensic.client)
+      }, bus.config.forensic)
       var connectionUrl = config.protocol + '://' + config.host
       if (config.port) {
         connectionUrl += ':' + config.port
@@ -14,25 +15,30 @@ module.exports = {
       if (config.path) {
         connectionUrl += config.path
       }
-      var client = new Ws(connectionUrl)
-      client.on('open', function open () {
-        // console.log('socket connection opened')
-        logMessage = function (msg) {
-          client.send(JSON.stringify(msg))
-        }
-      })
-      client.on('close', function open () {
-        // console.log('socket connection closed')
-        logMessage = null
-      })
-      client.on('error', function open (e) {
-        // console.log('socket error', e)
-        logMessage = null
-      })
+      var client
+      var connect = function () {
+        client = new Ws(connectionUrl)
+        client.on('open', function open () {
+          // console.log('socket connection opened')
+          logMessage = function (msg) {
+            client.send(msg)
+          }
+        })
+        client.on('close', function open () {
+          // console.log('socket connection closed')
+          logMessage = null
+          config.reconnectInterval && setTimeout(connect, config.reconnectInterval)
+        })
+        client.on('error', function open (e) {
+          // console.log('socket error', e)
+          logMessage = null
+        })
+      }
+      connect()
     }
   },
   log: function (msg) {
-    logMessage && logMessage(msg)
+    logMessage && logMessage(JSON.stringify(msg))
     return msg
   }
 }
