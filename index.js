cdae32f60e8c9dd5a9e56333ec5136f334fa1d80
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-05-04 19:37:23 +0300

    fix: linting

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = require('ut-run')
 .run({}, module)
 .then(runTasks)
 .then((app) => {
-    var id = 1
-    setInterval(() => {
-        app.bus.importMethod('forensic.log')({id: id++})
-    }, 1000)
+  var id = 1
+  setInterval(() => {
+    app.bus.importMethod('forensic.log')({id: id++})
+  }, 1000)
 })
