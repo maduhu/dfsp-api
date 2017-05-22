989c77a54c31e1addf4fb63f4f5c7146f6721f59
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-05-22 14:28:31 +0300

    fix: fire up peer implementaions from index_test

--- index_test.js
+++ index_test.js
@@ -3,3 +3,25 @@ module.exports = require('ut-run').run({
   method: 'debug',
   env: 'test'
 }, module)
+.then((app) => {
+  return Promise.all([
+    require('@leveloneproject/dfsp-directory/index_test'),
+    require('@leveloneproject/dfsp-rule/index_test'),
+    require('@leveloneproject/dfsp-transfer/index_test'),
+    require('@leveloneproject/dfsp-ledger/index_test'),
+    require('@leveloneproject/dfsp-identity/index_test'),
+    require('@leveloneproject/dfsp-account/index_test'),
+    require('@leveloneproject/dfsp-subscription/index_test'),
+    require('@leveloneproject/dfsp-mock')
+  ])
+  .catch(() => {
+    process.exit(1)
+  })
+  .then((apps) => {
+    return {
+      stop: () => {
+        apps.concat(app).forEach((x) => x.stop())
+      }
+    }
+  })
+})
