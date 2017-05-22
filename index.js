07acd9e39a53c58b1ece410c3e8ff2cbbd863456
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-05-22 16:23:53 +0300

    fix: add exception in case the forensic servise is down when the api is starting

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var runTasks = require('./tasks')
 module.exports = require('ut-run')
 .run({}, module)
 .then(runTasks)
-// .then((app) => {
-//   app.bus.importMethod('forensic.log')({
-//     message: 'DFSP up and running',
-//     config: app.config
-//   })
-// })
+.then((app) => {
+  app.bus.importMethod('forensic.log')({
+    message: 'DFSP up and running',
+    config: app.config
+  })
+  .catch((e) => {
+    return app
+  })
+})
