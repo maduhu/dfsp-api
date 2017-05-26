a1b98d27631f78b26db483ad221eb6dfb9dcdea9
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-05-26 18:44:30 +0300

    fix: build

--- test/integration/test.wallet.add.js
+++ test/integration/test.wallet.add.js
@@ -1,9 +1,18 @@
 /* eslint no-console: 0 */
 var test = require('ut-run/test')
-// var commonFunc = require('./../lib/commonFunctions.js')
-// var joi = require('joi')
+var commonFunc = require('./../lib/commonFunctions.js')
+var joi = require('joi')
 var config = require('./../lib/appConfig')
-
+const merchant = {
+  firstName: 'firstname' + commonFunc.generateRandomNumber(),
+  lastName: 'lastname' + commonFunc.generateRandomNumber(),
+  dob: '10/12/1999',
+  nationalId: '' + commonFunc.generateRandomNumber(),
+  phoneNumber: '' + commonFunc.generateRandomNumber(),
+  accountName: 'acc_' + commonFunc.generateRandomNumber(),
+  password: '1234',
+  roleName: 'merchant'
+}
 test({
   type: 'integration',
   name: 'DFSP API service',
@@ -17,18 +26,23 @@ test({
       // home screen
       name: 'Add merchant',
       method: 'wallet.add',
-      params: {
-        firstName: 'firstname',
-        lastName: 'lastname',
-        dob: '10/12/1999',
-        nationalId: '123456789',
-        phoneNumber: '987654321',
-        accountName: 'firstnamelastname',
-        password: '1234',
-        roleName: 'merchant'
-      },
+      params: merchant,
       result: (result, assert) => {
-        assert.equals(1, 1)
+        assert.equals(joi.validate(result, joi.object().keys({
+          account: 'http://localhost:8014/ledger/accounts/' + merchant.accountName,
+          accountName: joi.string().valid(merchant.accountName),
+          accountNumber: joi.string().valid(merchant.accountName),
+          actorId: joi.string().required(),
+          currency: joi.string().required(),
+          dob: joi.string().valid('10/12/1999'),
+          firstName: joi.string().valid(merchant.firstName),
+          identifier: joi.string().required(),
+          lastName: joi.string().valid(merchant.lastName),
+          nationalId: joi.string().valid(merchant.nationalId),
+          password: joi.string().valid('1234'),
+          phoneNumber: joi.string().valid(merchant.phoneNumber),
+          roleName: joi.string().valid('merchant')
+        })).error, null)
       }
     }])
   }
