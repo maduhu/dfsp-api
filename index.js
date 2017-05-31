8298d476c08ccbc9612ffeb33f1bdb1a32a2e64a
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-05-31 14:19:43 +0300

    fix: refactor config log - remove streams

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var runTasks = require('./tasks')
 module.exports = require('ut-run')
-.run({}, module)
-.then(runTasks)
-.then((app) => {
-  return app.bus.importMethod('forensic.log')({
-    message: 'DFSP Api up and running',
-    config: app.config
+  .run({}, module)
+  .then(runTasks)
+  .then((app) => {
+    return app.bus.importMethod('forensic.log')({
+      message: 'DFSP Api up and running',
+      config: JSON.parse(JSON.stringify(app.config, function (key, value) {
+        if (key === 'stream') {
+          return value.constructor.name
+        }
+        return value
+      }))
+    })
+      .then(() => app)
+      .catch(() => app)
   })
-  .then(() => app)
-  .catch(() => app)
-})
