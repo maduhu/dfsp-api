9f95149a90e29d38b26ffc209fc992152677bf52
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-07-07 15:52:40 +0300

    fix: central directory identifier

--- httpclient/ist/index.js
+++ httpclient/ist/index.js
@@ -24,8 +24,7 @@ module.exports = {
         'Authorization': 'Basic ' + new Buffer(this.config.key + ':' + this.config.secret).toString('base64')
       },
       qs: {
-        identifier: msg.identifier,
-        identifierType: 'eur'
+        identifier: msg.identifier + ':eur'
       }
     }
   },
