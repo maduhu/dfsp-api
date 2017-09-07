bc8b16132a63ee3679c1a1938d8bce58a9368643
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-09-07 15:53:29 +0300

    fix: update quotation object name

--- service/dfsp/ledger/transfer.notify.js
+++ service/dfsp/ledger/transfer.notify.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   'transfer.notify': function (msg, $meta) {
     return this.bus.importMethod('ledger.quote.edit')(
-      Object.assign({paymentId: msg.paymentId}, msg.quote)
+      Object.assign({paymentId: msg.paymentId}, msg.data)
     )
     .catch(() => false)
     .then((quote) => {
